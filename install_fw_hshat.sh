90283b8ba960b166cf808c065c4ad1fe8382bb73
VR2VYE <35602032+VR2VYE@users.noreply.github.com>
2019-03-07 22:39:20 +0800

    Update install_fw_hshat.sh

--- install_fw_hshat.sh
+++ install_fw_hshat.sh
@@ -73,4 +73,4 @@ fi
 sudo killall MMDVMHost >/dev/null 2>&1
 
 # Upload the firmware
-eval sudo $STM32FLASH -v -w mmdvm_hs_hat_fw.bin -g 0x0 -R -i 20,-21,21:-20,21 /dev/ttyAMA0
+eval sudo $STM32FLASH -v -w nano_hotspot_fw.bin -g 0x0 -R -i 20,-21,21:-20,21 /dev/ttyAMA0
